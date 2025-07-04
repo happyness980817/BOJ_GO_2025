b969a3fc4420f59d0de7866a27686d9e526da465
happyness980817 <happyness980817@gmail.com>
2025-07-04 22:00:37 +0900

    Problem 1330 - 두 수 비교하기

--- main.go
+++ main.go
@@ -1,14 +1,17 @@
-// Problem 10171 - 고양이
+// Problem 1330 - 두 수 비교하기
 
 package main
 
 import "fmt"
 
 func main() {
-	fmt.Println(`\    /\
- )  ( ')
-(  /  )
- \(__)|`)
+	var A, B int
+	fmt.Scan(&A, &B)
+	if A == B {
+		fmt.Println("==")
+	} else if A > B {
+		fmt.Println(">")
+	} else {
+		fmt.Println("<")
+	}
 }
-
-// 백틱 : raw string
