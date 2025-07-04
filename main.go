2b0968a10cf0f9dc2252b03171335d77ca83bee8
happyness980817 <happyness980817@gmail.com>
2025-07-04 22:11:16 +0900

    Problem 9498 - 시험 성적

--- main.go
+++ main.go
@@ -1,17 +1,22 @@
-// Problem 1330 - 두 수 비교하기
+// Problem 9498 - 시험 성적
 
 package main
 
 import "fmt"
 
 func main() {
-	var A, B int
-	fmt.Scan(&A, &B)
-	if A == B {
-		fmt.Println("==")
-	} else if A > B {
-		fmt.Println(">")
+	var score int
+	fmt.Scan(&score)
+
+	if score >= 90 && score <= 100 {
+		fmt.Println("A")
+	} else if score >= 80 {
+		fmt.Println("B")
+	} else if score >= 70 {
+		fmt.Println("C")
+	} else if score >= 60 {
+		fmt.Println("D")
 	} else {
-		fmt.Println("<")
+		fmt.Println("F")
 	}
 }
