5c1bf90c5a19f0d83e9df011f2aaa154ce54c8e5
happyness980817 <happyness980817@gmail.com>
2025-07-05 00:08:54 +0900

    Problem 2753 - 윤년 (2)

--- main.go
+++ main.go
@@ -8,11 +8,7 @@ func main() {
 	var Year int
 	fmt.Scan(&Year)
 
-	if Year%4 == 0 && Year%100 != 0 {
-		fmt.Println(1)
-	} else if Year%100 == 0 && Year%400 != 0 {
-		fmt.Println(0)
-	} else if Year%400 == 0 {
+	if (Year%4 == 0 && Year%100 != 0) || (Year%400 == 0) {
 		fmt.Println(1)
 	} else {
 		fmt.Println(0)
