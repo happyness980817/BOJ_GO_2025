57648369596ea94ed568a9ab1968a2ce53fbd73b
happyness980817 <happyness980817@gmail.com>
2025-07-04 20:04:11 +0900

    Problem 10172 - 개

--- main.go
+++ main.go
@@ -1,12 +1,13 @@
-// Problem 10171 - 고양이
+// Problem 10172 - 개
 
 package main
 
 import "fmt"
 
 func main() {
-	fmt.Println(`\    /\`)
-	fmt.Println(" )  ( ')")
-	fmt.Println("(  /  )")
-	fmt.Println(` \(__)|`)
+	fmt.Println(`|\_/|`)
+	fmt.Println(`|q p|   /}`)
+	fmt.Println(`( 0 )"""\`)
+	fmt.Println("|\"^\"`    |") // '\' : escape
+	fmt.Println(`||_/=\\__|`)
 }
